42f87742303a08c211b525a67d257e2523af29b4
HosungJoun <hosungjoun@gmail.com>
2024-03-09 14:51:42 +0900

    REDIS 추천값 LIST형태로 받아오게 수정

--- Yeaha3/src/main/java/com/smhrd/yeaha/controller/RecommendationController.java
+++ Yeaha3/src/main/java/com/smhrd/yeaha/controller/RecommendationController.java
@@ -1,11 +1,6 @@
 package com.smhrd.yeaha.controller;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,58 +8,60 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import redis.clients.jedis.Jedis;
+import java.util.List;
 
 @SessionAttributes("recommendationTextsFromRedis")
 @EnableSpringDataWebSupport
 @Controller
 public class RecommendationController {
 
-	@RequestMapping(value = "/show", method = RequestMethod.GET)
-	public String integrateAndShow(Model model) {
-		// Connect to Flask application and get recommendation texts
-		String flaskAppUrl = "http://localhost:5005";
-		String flaskResultEndpoint = "/";
+    @RequestMapping(value = "/show", method = RequestMethod.GET)
+    public String integrateAndShow(Model model) {
+        // Connect to Flask application and get recommendation texts
+        String flaskAppUrl = "http://localhost:5005";
+        String flaskResultEndpoint = "/";
 
-		// Build the full URL with query parameters
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(flaskAppUrl + flaskResultEndpoint)
-				.queryParam("val1", 1).queryParam("val2", 0).queryParam("val3", 1).queryParam("val4", 0);
+        // Build the full URL with query parameters
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(flaskAppUrl + flaskResultEndpoint)
+                .queryParam("val1", 1)
+                .queryParam("val2", 0)
+                .queryParam("val3", 1)
+                .queryParam("val4", 0);
 
-		String fullUrl = builder.toUriString();
-		System.out.println(fullUrl);
-		String recommendationTextsFromFlask = new RestTemplate().getForObject(fullUrl, String.class);
+        String fullUrl = builder.toUriString();
+        System.out.println(fullUrl);
+        String recommendationTextsFromFlask = new RestTemplate().getForObject(fullUrl, String.class);
 
-		// Connect to Redis
-		Jedis jedis = new Jedis("127.0.0.1", 6379);
-		// jedis.auth("1234");
-		// Get recommendation texts from Redis
-		String recommendationTextsFromRedis = jedis.get("recommend_texts");
-		System.out.println("제디스 닫기 전 Recommendation Texts from Flask: " + recommendationTextsFromFlask);
-		model.addAttribute("제디스 닫기 전 recommendationTextsFromRedis", recommendationTextsFromRedis);
+        // Connect to Redis
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
 
-		// Close Redis connection
-		jedis.close();
+        // Get recommendation texts from Redis as a list
+        List<String> recommendationTextsFromRedis = jedis.lrange("recommend_texts", 0, -1);
 
-		// Add recommendationTexts to the model
-		model.addAttribute("recommendationTextsFromFlask", recommendationTextsFromFlask);
-		model.addAttribute("recommendationTextsFromRedis", recommendationTextsFromRedis);
+        // Close Redis connection
+        jedis.close();
 
-		// Print to console (for verification)
-		System.out.println("Recommendation Texts from Flask: " + recommendationTextsFromFlask);
-		System.out.println("Recommendation Texts from Redis: " + recommendationTextsFromRedis);
+        // Add recommendationTexts to the model
+        model.addAttribute("recommendationTextsFromFlask", recommendationTextsFromFlask);
+        model.addAttribute("recommendationTextsFromRedis", recommendationTextsFromRedis);
 
-		return "redirect:/recommendationPage";
-	}
+        // Print to console (for verification)
+        System.out.println("Recommendation Texts from Flask: " + recommendationTextsFromFlask);
+        System.out.println("Recommendation Texts from Redis: " + recommendationTextsFromRedis);
 
-	@RequestMapping(value = "/recommendationPage", method = RequestMethod.GET)
-	public String showRecommendationPage() {
-		Jedis jedis = new Jedis("127.0.0.1", 6379);
-		String recommendationTextsFromRedis = jedis.get("recommend_texts");
-		System.out.println(recommendationTextsFromRedis);
-		
-		jedis.close();
+        return "redirect:/recommendationPage";
+    }
 
-		return "recommendationPage";
-	}
+    @RequestMapping(value = "/recommendationPage", method = RequestMethod.GET)
+    public String showRecommendationPage() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+
+        // Get recommendation texts from Redis as a list
+        List<String> recommendationTextsFromRedis = jedis.lrange("recommend_texts", 0, -1);
+
+        jedis.close();
+
+        return "recommendationPage";
+    }
 }
